a633523fa3bfde8f70ee9e3b0cfec0e79f66391f
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-09-01 07:56:01 +0530

    Loginpage code for master brnach

---
+++ src/test/java/gitPackage/LoginClass.java
@@ -0,0 +1,12 @@
+package gitPackage;
+
+public class LoginClass 
+{
+	public void loginPage()
+	{
+		System.out.println("Enter the username");
+		System.out.println("Enter the Password");
+		System.out.println("Click on login button");
+	}
+
+}
